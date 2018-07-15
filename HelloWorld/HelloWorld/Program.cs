d8ade9e2d363d448c60e7bbc64ed915493dc27da
msunkist99 <msunkist99@gmail.com>
2018-07-15 13:46:33 -0500

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Title = "CoderGirl 1-1";
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+        }
+    }
+}
